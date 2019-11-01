8d37d016efe1ba93e0ae8190146984b1e9df904c
dmitriy365377 <tigr365377@gmail.com>
2019-11-01 13:49:31 +0300

    redux thunk

---
+++ src/components/Header/HeaderContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from "./Header";
+import * as axios from "axios";
+import { connect } from "react-redux";
+import { setAuthUserData } from "../../redux/auth-reducer";
+import { authAPI } from '../../api/api';
+
+class HeaderContainer extends React.Component {
+    componentDidMount() {
+        authAPI.me()
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    let { id, login, email } = response.data.data;
+                    this.props.setAuthUserData(id, email, login);
+                }
+            });
+    }
+
+    render() {
+        return <Header {...this.props} />
+    }
+}
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth,
+    login: state.auth.login,
+});
+
+export default connect(mapStateToProps, { setAuthUserData })(HeaderContainer);
